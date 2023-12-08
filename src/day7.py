a1feab911761eeb3dcd8075944de86dc52c4fc3a
TeKrop <tekrop@proton.me>
2023-12-08 22:47:17 +0100

    finished day 7

--- src/day7.py
+++ src/day7.py
@@ -1,23 +1,26 @@
 from collections import defaultdict
-from enum import Enum, auto
+from enum import auto, IntEnum
 from functools import cached_property
+from itertools import product
 
 from .utils import AbstractPuzzleSolver
 
+# Global constants
+CARDS_TYPES: set[str] = {str(i) for i in range(2, 10)} | {"T", "J", "Q", "K", "A"}
+SPECIAL_CARDS_STRENGTH = {"T": 10, "J": 11, "Q": 12, "K": 13, "A": 14}
+CARDS_STRENGTH_MAPPING = {
+    card_type: (
+        int(card_type) if card_type.isdigit() else SPECIAL_CARDS_STRENGTH[card_type]
+    )
+    for card_type in CARDS_TYPES
+}
 
 class PuzzleSolver(AbstractPuzzleSolver):
     ###########################
     # DAY 7 - Common code
     ###########################
-    cards_strength_mapping: dict[str, int] = {
-        "T": 10,
-        "Q": 12,
-        "K": 13,
-        "A": 14,
-    }
-
-    def __calculate_total_winnings(self):
-        hands = sorted(Hand(line=line) for line in self.lines)
+    @staticmethod
+    def __calculate_total_winnings(hands: list["Hand"]):
         return sum(hand.bid * i for i, hand in enumerate(hands, start=1))
 
     ###########################
@@ -26,8 +29,9 @@ class PuzzleSolver(AbstractPuzzleSolver):
 
     def _solve_first_part(self) -> int:
         # Jack card for first part, better than T but lower than Q
-        self.cards_strength_mapping["J"] = 11
-        return self.__calculate_total_winnings()
+        CARDS_STRENGTH_MAPPING["J"] = 11
+        hands = sorted(Hand(line=line) for line in self.lines)
+        return self.__calculate_total_winnings(hands)
 
     ###########################
     # DAY 7 - Second Part
@@ -35,22 +39,21 @@ class PuzzleSolver(AbstractPuzzleSolver):
 
     def _solve_second_part(self) -> int:
         # Jack card is now a Joker, and its value is lower than 2
-        self.cards_strength_mapping["J"] = 1
-        return self.__calculate_total_winnings()
+        CARDS_STRENGTH_MAPPING["J"] = 1
+        hands = sorted(Hand(line=line, joker=True) for line in self.lines)
+        return self.__calculate_total_winnings(hands)
 
 
 class Card:
-    name: str
-
     def __init__(self, name: str):
         self.name = name
 
     @cached_property
     def strength(self) -> int:
-        return PuzzleSolver.cards_strength_mapping.get(self.name) or int(self.name)
+        return CARDS_STRENGTH_MAPPING[self.name]
 
     def __repr__(self):
-        return f"Card({self.name})"
+        return self.name
 
     def __eq__(self, other: "Card") -> bool:
         return self.strength == other.strength
@@ -59,7 +62,12 @@ class Card:
         return self.strength < other.strength
 
 
-class HandType(Enum):
+ALL_CARDS_EXCEPT_JOKER: list[Card] = [
+    Card(card_type) for card_type in CARDS_TYPES if card_type != "J"
+]
+
+
+class HandType(IntEnum):
     HIGH_CARD = auto()
     ONE_PAIR = auto()
     TWO_PAIR = auto()
@@ -71,17 +79,51 @@ class HandType(Enum):
 
 class Hand:
     cards: list[Card]
-    bid: int
+    bid: int = 0
+    joker: bool = False
 
-    def __init__(self, line: str):
-        cards, bid = line.split()
-        self.cards = [Card(card_name) for card_name in cards]
-        self.bid = int(bid)
+    def __init__(self, line: str = None, joker: bool = False, cards: list[Card] = None):
+        if cards:
+            self.cards = cards
+        else:
+            cards, bid = line.split()
+            self.cards = [Card(card_name) for card_name in cards]
+            self.bid = int(bid)
+
+        self.joker = joker
 
     @cached_property
     def hand_type(self) -> HandType:
+        # Compute the hand type depending if J is a Joker
+        return (
+            self.__get_best_hand_type()
+            if self.joker
+            else self.__get_hand_type(self.cards)
+        )
+
+    def __eq__(self, other: "Hand") -> bool:
+        return self.cards == other.cards
+
+    def __lt__(self, other: "Hand") -> bool:
+        # First check hand types
+        if self.hand_type != other.hand_type:
+            return self.hand_type < other.hand_type
+
+        # Then, check cards individually, one after another
+        for self_card, other_card in zip(self.cards, other.cards):
+            if self_card != other_card:
+                return self_card < other_card
+
+        # Hands seems equal, so self is not lower than other
+        return False
+
+    def __repr__(self) -> str:
+        return f"Hand(cards={self.cards},bid={self.bid},hand_type={self.hand_type})"
+
+    @staticmethod
+    def __get_hand_type(cards: list[Card]) -> HandType:
         cards_by_type = defaultdict(int)
-        for card in self.cards:
+        for card in cards:
             cards_by_type[card.name] += 1
 
         different_card_types = len(cards_by_type)
@@ -124,22 +166,27 @@ class Hand:
         # All card labels are distinct
         return HandType.HIGH_CARD
 
-    @cached_property
-    def hand_type_strength(self) -> int:
-        return self.hand_type.value
+    def __get_best_hand_type(self) -> HandType:
+        # Retrieve the joker positions
+        joker_positions: set[int] = {
+            i for i, card in enumerate(self.cards) if card.name == "J"
+        }
 
-    def __eq__(self, other: "Hand") -> bool:
-        return self.cards == other.cards
+        # First, if we don't have any Joker, no need for calculations
+        if not joker_positions:
+            return self.__get_hand_type(self.cards)
 
-    def __lt__(self, other: "Hand") -> bool:
-        # First check hand types
-        if self.hand_type_strength != other.hand_type_strength:
-            return self.hand_type_strength < other.hand_type_strength
+        # Get a list of all possible hands
+        possible_hands = self.__get_possible_hands(joker_positions)
 
-        # Then, check cards individually, one after another
-        for self_card, other_card in zip(self.cards, other.cards):
-            if self_card != other_card:
-                return self_card < other_card
+        # Return the best possible hand type
+        return max(self.__get_hand_type(hand.cards) for hand in possible_hands)
 
-        # Hands seems equal, so self is not lower than other
-        return False
+    def __get_possible_hands(self, joker_positions: set[int]) -> list[Card]:
+        # First, assemble possible cards for each position, depending on joker positions
+        possible_cards = [
+            ALL_CARDS_EXCEPT_JOKER if i in joker_positions else [self.cards[i]]
+            for i in range(5)
+        ]
+
+        return [Hand(cards=cards) for cards in product(*possible_cards)]
